17f1fd09dd8bdfeb829adc4f5a2dc0cefa6741e5
Luseniik <lkromah@aggies.ncat.edu>
2016-03-26 01:57:54 -0400

    parse signin

--- app/src/main/java/com/example/newuser/whogotnext/ParseInit.java
+++ app/src/main/java/com/example/newuser/whogotnext/ParseInit.java
@@ -10,14 +10,18 @@ import com.parse.ParseUser;import java.lang.Override;
  * Created by newuser on 12/4/15.
  */
 public class ParseInit extends Application {
+
+    private final String  parse_app_id = "eyMzbtH56tAE2rMcm9TjBIvfSRI34uCDENAJ31ay";
+    private final String  parse_client_id = "bosjczf81WGIAteEFZvfAhsIufxi3YgzpLWUYUlC";
     @Override
+
     public void onCreate(){
         super.onCreate();
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
 
         // Add your initialization code here
-        Parse.initialize(this, "ctuz6QDJBBNXRDiSMcbPKvRstVONBhxop5ZUgO0c", "FYfnUGuXNAtfmMzj34TeJLUaBTMbEcoZ3GxF4TCb");
+        Parse.initialize(this, parse_app_id, parse_client_id);
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();
         // Optionally enable public read access.
