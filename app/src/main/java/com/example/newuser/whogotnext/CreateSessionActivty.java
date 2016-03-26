25be61ffd50242c2fb5070ee2cfb7bc5b3723273
Luseniik <lkromah@aggies.ncat.edu>
2016-03-26 06:43:41 -0400

    Changes

--- app/src/main/java/com/example/newuser/whogotnext/CreateSessionActivty.java
+++ app/src/main/java/com/example/newuser/whogotnext/CreateSessionActivty.java
@@ -1,13 +1,37 @@
 package com.example.newuser.whogotnext;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseObject;
 
 public class CreateSessionActivty extends AppCompatActivity {
 
+    private Button button;
+    private EditText text;
+    private EditText size;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_session_activty);
+
+        button = (Button) findViewById(R.id.button_create);
+        text = (EditText)findViewById(R.id.name);
+        size = (EditText) findViewById(R.id.size);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseObject object = new ParseObject("Session");
+                object.put("Name",text.getText().toString());
+                object.put("Size", Integer.parseInt(size.getText().toString()));
+                object.saveInBackground();
+                Intent toListOfSession = new Intent(getApplicationContext(),ListOfSessionActivity.class);
+                startActivity(toListOfSession);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newuser/whogotnext/ListOfSessionActivity.java
+++ app/src/main/java/com/example/newuser/whogotnext/ListOfSessionActivity.java
@@ -3,15 +3,26 @@ package com.example.newuser.whogotnext;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListOfSessionActivity extends AppCompatActivity {
 
     private FloatingActionButton fabButton;
+    List<Event> mList=new ArrayList<Event>();
+    SportsListAdapter arrayAdapter=null;
+    ListView sportsListView;
+    String objectId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,5 +43,27 @@ public class ListOfSessionActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    private void queryEvents() {
+        ParseQuery<Event> query = ParseQuery.getQuery("Session");
+        query.findInBackground(new FindCallback<Event>() {
+            @Override
+            public void done(List<Event> objectList, ParseException e) {
+                if (e == null) {
+                    mList = objectList;
+                    arrayAdapter = new SportsListAdapter(getApplicationContext(), objectId, mList);
+                    sportsListView.setAdapter(arrayAdapter);
+                    arrayAdapter.notifyDataSetChanged();
+
+                } else {
+                    Log.e("Error", e.toString());
+                    Toast.makeText(getApplicationContext(), "Error Loading Events", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
 
 }
